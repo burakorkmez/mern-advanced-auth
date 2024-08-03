46e235c65279e33d11d1d6438695dfe23e85e6a2
Burak Orkmez <burakorkmezz@gmail.com>
2024-08-04 01:19:08 +0300

    URL updated in authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 
 axios.defaults.withCredentials = true;
 
